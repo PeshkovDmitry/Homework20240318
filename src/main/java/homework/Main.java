66d75dbe9f36ad119ad6c1df532c59cdca34f9ca
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-03-21 14:30:15 +0300

    Реализована часть 1 домашнего задания

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -1,7 +1,85 @@
 package homework;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test")) {
+            createTable(connection);
+            insertValues(connection);
+            selectValues(connection);
+            System.out.println("-----------------------");
+            updateValues(connection);
+            selectValues(connection);
+            System.out.println("-----------------------");
+            deleteValues(connection);
+            selectValues(connection);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void deleteValues(Connection connection) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("delete from students where age = $1");
+        statement.setInt(1, 20);
+        statement.execute();
+    }
+
+    private static void updateValues(Connection connection) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("update students set age = $1 where first_name = $2");
+        statement.setInt(1, 20);
+        statement.setString(2, "Петр");
+        statement.execute();
+    }
+
+    private static void selectValues(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select id, first_name, second_name, age from students");
+        while (resultSet.next()) {
+            System.out.println(
+                    String.format(
+                            "%d: %s %s %d лет",
+                            resultSet.getInt(1),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getInt(4))
+            );
+        }
+    }
+
+    private static void insertValues(Connection connection) throws SQLException {
+        for (Student student:
+             getStudents()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into students (id, first_name, second_name, age) values ($1, $2, $3, $4)"
+            );
+            preparedStatement.setInt(1, student.getId());
+            preparedStatement.setString(2, student.getFirstName());
+            preparedStatement.setString(3, student.getSecondName());
+            preparedStatement.setInt(4, student.getAge());
+            preparedStatement.execute();
+        }
+    }
+
+    private static void createTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(
+                "create table students (id int, first_name varchar(256), second_name varchar(256), age int)"
+        );
+    }
+
+    private static List<Student> getStudents() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Иван", "Иванов", 19));
+        list.add(new Student(2, "Петр", "Петров", 18));
+        list.add(new Student(3, "Татьяна", "Семенова", 19));
+        list.add(new Student(4, "Алла", "Сидорова", 18));
+        return list;
+    }
+
+
 }
