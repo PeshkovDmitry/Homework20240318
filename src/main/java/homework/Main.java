0ba8cf34c7ab5899b4311385e00a08c43afa4c08
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-03-22 06:20:15 +0300

    Доделал задание 2

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -7,39 +7,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-//        connectToH2();
-
-        /**
-         * Настройка PostgreSQL
-         * в BASH: sudo -i -u postgres
-         * в BASH: psql
-         * в POSTGRESQL: create user test with password 'test' superuser;
-         * в POSTGRESQL: create database test;
-         * Теперь есть пользователь test с паролем test,
-         * и есть база данных test
-         */
-
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost/test",
-                "test",
-                "test")) {
+        try (Connection connection = getPostgreSqlConnection()) {
             dropTable(connection);
-            createTable(connection);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void dropTable(Connection connection) throws SQLException {
-        connection.createStatement().execute(
-                "drop table if exists students"
-        );
-    }
-
-    private static void connectToH2() {
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test")) {
             createTable(connection);
             insertValues(connection);
             selectValues(connection);
@@ -54,17 +23,44 @@ public class Main {
         }
     }
 
-    private static void deleteValues(Connection connection) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement("delete from students where age = $1");
-        statement.setInt(1, 20);
-        statement.execute();
+    private static Connection getH2Connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:mem:test");
     }
 
-    private static void updateValues(Connection connection) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement("update students set age = $1 where first_name = $2");
-        statement.setInt(1, 20);
-        statement.setString(2, "Петр");
-        statement.execute();
+    private static Connection getPostgreSqlConnection() throws SQLException {
+//         Настройка PostgreSQL
+//         в BASH: sudo -i -u postgres
+//         в BASH: psql
+//         в POSTGRESQL: create user test with password 'test' superuser;
+//         в POSTGRESQL: create database test;
+//         Теперь есть пользователь test с паролем test,
+//         и есть база данных test
+        return DriverManager.getConnection("jdbc:postgresql://localhost/test","test","test");
+    }
+
+    private static void dropTable(Connection connection) throws SQLException {
+        connection.createStatement().execute("drop table if exists students");
+    }
+
+    private static void createTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(
+                "create table students (id int, first_name varchar(256), second_name varchar(256), age int)"
+        );
+    }
+
+    private static void insertValues(Connection connection) throws SQLException {
+        for (Student student:
+                getStudents()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into students (id, first_name, second_name, age) values (?, ?, ?, ?)"
+            );
+            preparedStatement.setInt(1, student.getId());
+            preparedStatement.setString(2, student.getFirstName());
+            preparedStatement.setString(3, student.getSecondName());
+            preparedStatement.setInt(4, student.getAge());
+            preparedStatement.execute();
+        }
     }
 
     private static void selectValues(Connection connection) throws SQLException {
@@ -82,25 +78,17 @@ public class Main {
         }
     }
 
-    private static void insertValues(Connection connection) throws SQLException {
-        for (Student student:
-             getStudents()) {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "insert into students (id, first_name, second_name, age) values ($1, $2, $3, $4)"
-            );
-            preparedStatement.setInt(1, student.getId());
-            preparedStatement.setString(2, student.getFirstName());
-            preparedStatement.setString(3, student.getSecondName());
-            preparedStatement.setInt(4, student.getAge());
-            preparedStatement.execute();
-        }
+    private static void deleteValues(Connection connection) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("delete from students where age = ?");
+        statement.setInt(1, 20);
+        statement.execute();
     }
 
-    private static void createTable(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.execute(
-                "create table students (id int, first_name varchar(256), second_name varchar(256), age int)"
-        );
+    private static void updateValues(Connection connection) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("update students set age = ? where first_name = ?");
+        statement.setInt(1, 20);
+        statement.setString(2, "Петр");
+        statement.execute();
     }
 
     private static List<Student> getStudents() {
