652f83bb3d3b35cd28c28c90034a8a3b4b115deb
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-03-22 06:23:18 +0300

    Доделал задание 2

--- src/main/java/homework/Student.java
+++ src/main/java/homework/Student.java
@@ -1,12 +1,5 @@
 package homework;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Student {
 
     private int id;
@@ -17,4 +10,55 @@ public class Student {
 
     private int age;
 
+    public Student() {
+    }
+
+    public Student(int id, String firstName, String secondName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
